de8a11064e8fa9f817701c70a6832e5f49737817
14004923 <safaei.b@entekhabelectronic.com>
2023-07-02 10:22:15 +0330

    first commit

---
+++ src/components/Data.js
@@ -0,0 +1,17 @@
+export const userList = [
+    {
+        name:'rozhin',
+        email:'rozhin@gmail.com',
+        id:1,
+    },
+    {
+        name:'arshiya',
+        email:'arshiya@gmail.com',
+        id:2,
+    },
+    {
+        name:'armin',
+        email:'armin@gmail.com',
+        id:3,
+    },
+]
